06deffbf14c9d28641e370991a4ddaacdc1a84f0
Hannes Harnisch <hannes.harnisch@gmx.net>
2019-09-27 11:03:26 +0200

    Add project files.

---
+++ Fractarium/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fractarium.ViewModels
+{
+	public class MainWindowViewModel : ViewModelBase
+	{
+		public string Greeting => "Welcome to Avalonia!";
+	}
+}
