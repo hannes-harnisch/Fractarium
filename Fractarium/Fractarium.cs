f6d3ab9be98e64fd5590bf02766dea13f80ef978
Hannes Harnisch <hannes.harnisch@gmx.net>
2019-09-27 11:34:05 +0200

    Initial commit

---
+++ Fractarium/Fractarium.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Logging.Serilog;
+
+using Fractarium.ViewModels;
+using Fractarium.Views;
+
+namespace Fractarium
+{
+	public class Fractarium
+	{
+		public static void Main(string[] args)
+		{
+			AppBuilder.Configure<App>().UsePlatformDetect().LogToDebug().UseReactiveUI().Start(App, args);
+		}
+
+		private static void App(Application app, string[] args)
+		{
+			app.Run(new MainWindow { DataContext = new MainWindowViewModel() });
+		}
+	}
+}
--- Fractarium/ViewLocator.cs
+++ Fractarium/ViewLocator.cs
@@ -1,6 +1,8 @@
 using System;
+
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
+
 using Fractarium.ViewModels;
 
 namespace Fractarium
@@ -11,17 +13,13 @@ namespace Fractarium
 
 		public IControl Build(object data)
 		{
-			var name = data.GetType().FullName.Replace("ViewModel", "View");
+			string name = data.GetType().FullName.Replace("ViewModel", "View");
 			var type = Type.GetType(name);
 
 			if(type != null)
-			{
 				return (Control)Activator.CreateInstance(type);
-			}
 			else
-			{
 				return new TextBlock { Text = "Not Found: " + name };
-			}
 		}
 
 		public bool Match(object data)
