53af2fa4b36179b7cc2d002e3a93ea047cd11783
Hannes Harnisch <hannes.harnisch@gmx.net>
2019-09-30 03:39:27 +0200

    Finished parameter error handling

---
+++ Fractarium/Logic/ComplexUtil.cs
@@ -0,0 +1,84 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace Fractarium.Logic
+{
+	public static class ComplexUtil
+	{
+		private const string Pattern = @"^(-|\+)?(([0-9]+(\.[0-9]+)?)?i|[0-9]+(\.[0-9]+)?((-|\+)([0-9]+(\.[0-9]+)?)?i)?)$";
+
+		public static bool TryParse(string s, out Complex result)
+		{
+			result = Complex.Zero;
+			bool valid = Regex.IsMatch(s, Pattern);
+			if(valid)
+				result = new Complex(double.Parse(ParseReal(s)), double.Parse(ParseImaginary(s)));
+			return valid;
+		}
+
+		private static string ParseReal(string s)
+		{
+			string real = "0";
+			bool negativeStart = false;
+			if(!s.Contains("i"))
+				real = s;
+			else
+			{
+				if(s.StartsWith("-"))
+					negativeStart = true;
+				if(s.StartsWith("-") || s.StartsWith("+"))
+					s = s.Substring(1);
+				if(s.Contains("+"))
+				{
+					real = s.Substring(0, s.IndexOf("+"));
+					if(negativeStart)
+						real = "-" + real;
+				}
+				else if(s.Contains("-"))
+				{
+					real = s.Substring(0, s.IndexOf("-"));
+					if(negativeStart)
+						real = "-" + real;
+				}
+			}
+			return real;
+		}
+
+		private static string ParseImaginary(string s)
+		{
+			string imaginary = "0";
+			bool negativeStart = false;
+			if(s.Contains("i"))
+			{
+				if(s.StartsWith("-"))
+					negativeStart = true;
+				if(s.StartsWith("-") || s.StartsWith("+"))
+					s = s.Substring(1);
+				if(s.Contains("+"))
+				{
+					if(string.IsNullOrEmpty(s.Substring(s.IndexOf("+") + 1, s.IndexOf("i") - s.IndexOf("+") - 1)))
+						imaginary = "1";
+					else
+						imaginary = s.Substring(s.IndexOf("+") + 1, s.IndexOf("i") - s.IndexOf("+") - 1);
+				}
+				else if(s.Contains("-"))
+				{
+					if(string.IsNullOrEmpty(s.Substring(s.IndexOf("-") + 1, s.IndexOf("i") - s.IndexOf("-") - 1)))
+						imaginary = "-1";
+					else
+						imaginary = s.Substring(s.IndexOf("-"), s.IndexOf("i") - s.IndexOf("-"));
+				}
+				else
+				{
+					if(string.IsNullOrEmpty(s.Substring(0, s.IndexOf("i"))))
+						imaginary = "1";
+					else
+						imaginary = s.Substring(0, s.IndexOf("i"));
+					if(negativeStart)
+						imaginary = "-" + imaginary;
+				}
+			}
+			return imaginary;
+		}
+	}
+}
--- Fractarium/UserInterface/ParameterTab.xaml.cs
+++ Fractarium/UserInterface/ParameterTab.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
+using Fractarium.Logic;
 using Fractarium.Logic.Fractals;
 
 namespace Fractarium.UserInterface
@@ -9,51 +12,58 @@ namespace Fractarium.UserInterface
 	{
 		public string[] TypeEntries => FractalTypes.Names;
 
-		public string FractalType { get; set; }
-
-		public bool BindImageSizeToWindow
-		{
-			get => _bindImageSizeToWindow;
-			set
-			{
-				_bindImageSizeToWindow = value;
-				System.Console.WriteLine(value);
-			}
-		}
-		private bool _bindImageSizeToWindow = true;
-
-		public string ImageWidth
-		{
-			set
-			{
-				WidthError = uint.TryParse(value, out uint result);
-				if(WidthError)
-					App.Parameters.Width = result;
-			}
-		}
-
-		public bool WidthError { get; set; }
-
-		public string ImageHeight { get; set; }
-
-		public string IterationLimit { get; set; }
-
-		public string ZoomFactor { get; set; }
-
-		public string Scale { get; set; }
-
-		public string Midpoint { get; set; }
-
-		public string JuliaConstant { get; set; }
-
-		public string PhoenixConstant { get; set; }
-
-		public string MultibrotExponent { get; set; }
-
 		public ParameterTab()
 		{
 			AvaloniaXamlLoader.Load(this);
 			DataContext = this;
+		}
+
+		public void BindImageSizeToWindow(object sender, RoutedEventArgs e)
+		{
+			System.Console.WriteLine(((CheckBox)sender).IsChecked);
+		}
+
+		public void SetTextBoxState(TextBox box, bool parsed)
+		{
+			box.Classes = new Classes(parsed ? "" : "Error");
+		}
+
+		public void OnPositiveIntInput(object sender, KeyEventArgs e)
+		{
+			bool parsed = uint.TryParse(((TextBox)sender).Text, out uint result);
+			SetTextBoxState((TextBox)sender, parsed);
+			if(parsed)
+				switch(((TextBox)sender).Name)
+				{
+					case "Width":
+						App.Parameters.Width = result; break;
+					case "Height":
+						App.Parameters.Height = result; break;
+					case "IterationLimit":
+						App.Parameters.IterationLimit = result; break;
+					case "ZoomFactor":
+						App.Parameters.ZoomFactor = result; break;
+				}
+		}
+
+		public void OnLongInput(object sender, KeyEventArgs e)
+		{
+			bool parsed = ulong.TryParse(((TextBox)sender).Text, out ulong result);
+			SetTextBoxState((TextBox)sender, parsed);
+			if(parsed)
+				App.Parameters.Scale = result;
+		}
+
+		public void OnComplexInput(object sender, KeyEventArgs e)
+		{
+			bool parsed = ComplexUtil.TryParse(((TextBox)sender).Text, out var result);
+			SetTextBoxState((TextBox)sender, parsed);
+			if(parsed)
+				switch(((TextBox)sender).Name)
+				{
+					case "Midpoint":
+						App.Parameters.Midpoint = result; break;
+				}
 		}
 	}
 }
